b034b96b50c0401c9c1bea6ea6fcab62a874a10c
William Barre <willbarre9@gmail.com>
2022-03-07 17:44:09 +0100

    premières variables

---
+++ helloworld.py
@@ -0,0 +1,18 @@
+livre = "Gatsby le Magnifique"
+livre = "On the road"
+print(livre)
+
+nombre = 2.2
+print(type(nombre))
+
+fruits = ["pomme", "banane", "poire"] # tableau = liste
+print(fruits[-1])
+print(fruits[0][2])
+fruits.append("orange")
+fruits.remove("banane")
+fruits.sort()
+print(fruits)
+print(len(fruits))
+
+un_tuple = ("visse", "clout", "pointe")
+print(un_tuple)
